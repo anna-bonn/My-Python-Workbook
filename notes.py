ba65b0b44ff2492beda0aa0678a3065e791bab4a
Anna Bonn <bonnanna08@gmail.com>
2025-08-22 15:26:41 +0000

    Main

---
+++ notes.py
@@ -0,0 +1,18 @@
+
+"""
+def is the variable
+whatType is the name of the function 
+userInput is the parameter of the function and the variable that the user inputs
+type is a built in function that labels the datatype 
+
+"""
+
+def whatType (userInput):
+    print(type(userInput))
+
+whatType (3)
+whatType (3.0)
+whatType ("anna")
+whatType (True)
+whatType ('a')
+
